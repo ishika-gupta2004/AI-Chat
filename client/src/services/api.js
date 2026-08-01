e93aef4a16f3a58e67bca6b3d4f253d80c4f8927
Ishika Gupta <pintu2636j@gmail.com>
2026-08-01 17:17:32 +0530

    Connect frontend to production backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create(
     {
-        baseURL: "http://localhost:5000/api"
+        baseURL: "https://https://ai-chat-810u.onrender.com.onrender.com/api"
     }
 );
 
