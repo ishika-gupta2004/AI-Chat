6aab02d808d2a5ee7a8c83855d4131b48081bb7b
Ishika Gupta <pintu2636j@gmail.com>
2026-08-01 17:50:04 +0530

    Deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create(
     {
-        baseURL: "https://https://ai-chat-810u.onrender.com.onrender.com/api"
+        baseURL: import.meta.env.VITE_API_URL
     }
 );
 
